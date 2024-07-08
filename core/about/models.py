513cd0eb6c1bf9e3b1ad6ec9d53a4e1c2f3aad9c
LCapomasi <lucapomasi@gmail.com>
2024-07-07 22:48:23 -0300

    creacion de models

---
+++ core/about/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField()
+    
