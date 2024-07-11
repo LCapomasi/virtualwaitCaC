8c5c0b9b5603ef365ef7d83ec9d03cc5da2810d8
RegiOli <167256752+RegiOli@users.noreply.github.com>
2024-07-11 18:24:04 -0300

    Update apps.py

--- about/apps.py
+++ about/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class AboutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'about'
-    verbose_name = 'SobreNosotros'
+    name = 'reservaturnos'
+    verbose_name = 'reservaturnos'
